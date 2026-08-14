42a41b90ee3b5b06f7d2a0edfe8de5fb39b986dd
firsthugs <firsthugscollection@gmail.com>
2026-08-14 12:55:40 +0800

    Update products.js

--- products.js
+++ products.js
@@ -1,39 +1,20 @@
 const products = [
+
   {
     name: "Mesh Dress",
     price: 150,
     image: "mesh-dress.webp",
-    sizes: [
-      "3-6M",
-      "6-9M",
-      "9-12M",
-      "12-18M",
-      "1.5-2Y",
-      "2-3Y",
-      "3-4Y",
-      "4-5Y"
-    ]
-  },
 
-  {
-    name: "Baby Sleepsuit",
-    price: 35,
-    image: "sleepsuit.webp",
     sizes: [
-      "3-6M",
-      "6-9M",
-      "9-12M"
-    ]
-  },
-
-  {
-    name: "Co-ord Set",
-    price: 78,
-    image: "coord-set.webp",
-    sizes: [
-      "1-2Y",
-      "2-3Y",
-      "3-4Y"
+      { size: "3-6M", available: true },
+      { size: "6-9M", available: true },
+      { size: "9-12M", available: true },
+      { size: "12-18M", available: true },
+      { size: "1.5-2Y", available: true },
+      { size: "2-3Y", available: true },
+      { size: "3-4Y", available: true },
+      { size: "4-5Y", available: true }
     ]
   }
+
 ];
