f7c0d67695a015293a34f91a775437e48ae41fa5
firsthugs <firsthugscollection@gmail.com>
2026-08-14 12:58:31 +0800

    Refactor size availability entries in products.js

--- products.js
+++ products.js
@@ -2,19 +2,55 @@ const products = [
 
   {
     name: "Mesh Dress",
+
     price: 150,
+
     image: "mesh-dress.webp",
 
     sizes: [
-      { size: "3-6M", available: true },
-      { size: "6-9M", available: true },
-      { size: "9-12M", available: true },
-      { size: "12-18M", available: true },
-      { size: "1.5-2Y", available: true },
-      { size: "2-3Y", available: true },
-      { size: "3-4Y", available: true },
-      { size: "4-5Y", available: true }
+
+      {
+        size: "3-6M",
+        available: true
+      },
+
+      {
+        size: "6-9M",
+        available: true
+      },
+
+      {
+        size: "9-12M",
+        available: true
+      },
+
+      {
+        size: "12-18M",
+        available: true
+      },
+
+      {
+        size: "1.5-2Y",
+        available: true
+      },
+
+      {
+        size: "2-3Y",
+        available: true
+      },
+
+      {
+        size: "3-4Y",
+        available: true
+      },
+
+      {
+        size: "4-5Y",
+        available: true
+      }
+
     ]
+
   }
 
 ];
