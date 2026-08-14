a458b315d19b9b8e28948a4a819b88ad699e85a8
firsthugs <firsthugscollection@gmail.com>
2026-08-14 13:37:39 +0800

    Update products.js

--- products.js
+++ products.js
@@ -2,65 +2,28 @@ const products = [
 
   {
     name: "Mesh Dress",
-
     price: 150,
-
     image: "mesh-dress.webp",
 
     sizes: [
-
-      {
-        size: "3-6M",
-        available: false
-      },
-
-      {
-        size: "6-9M",
-        available: false
-      },
-
-      {
-        size: "9-12M",
-        available: true
-      },
-
-      {
-        size: "12-18M",
-        available: false
-      },
-
-      {
-        size: "1.5-2Y",
-        available: false
-      },
-
-      {
-        size: "2-3Y",
-        available: false
-      },
-
-      {
-        size: "3-4Y",
-        available: false
-      },
-
-      {
-        size: "4-5Y",
-        available: false
-      }
-
+      { size: "3-6M", available: false },
+      { size: "6-9M", available: false },
+      { size: "9-12M", available: true },
+      { size: "12-18M", available: false },
+      { size: "1.5-2Y", available: false },
+      { size: "2-3Y", available: false },
+      { size: "3-4Y", available: false },
+      { size: "4-5Y", available: false }
     ]
-
   },
-  const products = [
 
   {
-    name: "Neutral Embroidered Top abd Pants",
-    price: RM 125,
+    name: "Neutral Embroidered Top and Pants",
+    price: 125,
     image: "set.jpg",
 
     sizes: [
-      { size: "1.5-2y", available: true },
+      { size: "1.5-2Y", available: true },
       { size: "6-9M", available: false }
     ]
   }
