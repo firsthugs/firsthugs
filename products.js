18f12205cfdc3acb10fda126532a247f179c1113
firsthugs <firsthugscollection@gmail.com>
2026-08-14 12:48:26 +0800

    Add product list with details
    Added an array of products with details including name, price, image, and available sizes.

--- products.js
+++ products.js
@@ -1,1 +1,39 @@
+const products = [
+  {
+    name: "Mesh Dress",
+    price: 150,
+    image: "mesh-dress.webp",
+    sizes: [
+      "3-6M",
+      "6-9M",
+      "9-12M",
+      "12-18M",
+      "1.5-2Y",
+      "2-3Y",
+      "3-4Y",
+      "4-5Y"
+    ]
+  },
 
+  {
+    name: "Baby Sleepsuit",
+    price: 35,
+    image: "sleepsuit.webp",
+    sizes: [
+      "3-6M",
+      "6-9M",
+      "9-12M"
+    ]
+  },
+
+  {
+    name: "Co-ord Set",
+    price: 78,
+    image: "coord-set.webp",
+    sizes: [
+      "1-2Y",
+      "2-3Y",
+      "3-4Y"
+    ]
+  }
+];
